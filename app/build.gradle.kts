41c63f18c03f436cbd6c6cf066e1a61d44b327e6
Leydy Macareo <lmacareo456@unab.edu.co>
2025-04-10 16:57:29 -0500

    Configuracion Navegacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    val nav_version = "2.8.9"
+    //Esta es la libreria de navegacion
+    implementation(libs.androidx.navigation.compose)
 }
--- app/src/main/java/com/leydymacareo/ecomerceapp/MainActivity.kt
+++ app/src/main/java/com/leydymacareo/ecomerceapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.leydymacareo.ecomerceapp
 
+import LoginSreen
+import RegisterScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.leydymacareo.ecomerceapp.ui.theme.EcomerceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +25,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EcomerceAppTheme {
-
-
+                val myNavController = rememberNavController()
+                val myStarDestination: String = "login"
+                NavHost(
+                    navController = myNavController,
+                    startDestination = myStarDestination,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    composable ("login"){
+                        LoginSreen()
+                    }
+                    composable ("register") { RegisterScreen() }
+                }
             }
         }
     }
